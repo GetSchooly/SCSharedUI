0cadb3e02d3283a8d24fed4a89cdda87a1de6bd2
Ankit Kumar <akki6230@hotmail.com>
2025-05-29 23:21:06 +0530

    fixed issue with event

--- Sources/SCSharedUI/Sources/Production/Event/EventListViewModel.swift
+++ Sources/SCSharedUI/Sources/Production/Event/EventListViewModel.swift
@@ -4,7 +4,7 @@ import SwiftUI
 public class EventListViewModel: ObservableObject {
     var eventList: [EventDetailModel] = []
     
-    static let sample: EventDetailModel = EventDetailModel(name: "Independance Day", date: "15th August", time: "07:15 AM", view: "36.45K", image: "https://picsum.photos/id/103/200/200")
+    public static let sample: EventDetailModel = EventDetailModel(name: "Independance Day", date: "15th August", time: "07:15 AM", view: "36.45K", image: "https://picsum.photos/id/103/200/200")
     
     public init() {
         self.eventList = [ EventDetailModel(name: "Deepawali", date: "20th Nov 2019", time: "07:15 PM", view: "36.45K", image: "https://picsum.photos/id/103/200/200"),
