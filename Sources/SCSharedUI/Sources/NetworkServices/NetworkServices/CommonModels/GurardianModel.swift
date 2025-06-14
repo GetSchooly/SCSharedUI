8c9f5ea7e829be3dcb7537b342775c068d545984
Ankit Kumar <akki6230@hotmail.com>
2025-06-15 00:33:47 +0530

    fixed common model

--- Sources/SCSharedUI/Sources/NetworkServices/NetworkServices/CommonModels/GurardianModel.swift
+++ Sources/SCSharedUI/Sources/NetworkServices/NetworkServices/CommonModels/GurardianModel.swift
@@ -5,4 +5,10 @@ open class GurardianModel: Object, Codable {
     @Persisted(primaryKey: true) public var gurardianEmailId: String
     @Persisted var token: String
     @Persisted var gurardianName: String
+    
+    enum CodingKeys: String, CodingKey {
+        case gurardianEmailId = "GurardianEmailId"
+        case token
+        case gurardianName = "GurardianName"
+    }
 }
