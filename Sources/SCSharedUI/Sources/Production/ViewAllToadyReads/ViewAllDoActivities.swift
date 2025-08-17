f678c991de9c8b305c711b5cab3f38c7fc3a78c5
Ankit Kumar <akki6230@hotmail.com>
2025-08-17 13:00:00 +0530

    Fixes UI DIY and Today's read Listing

--- Sources/SCSharedUI/Sources/Production/ViewAllToadyReads/ViewAllDoActivities.swift
+++ Sources/SCSharedUI/Sources/Production/ViewAllToadyReads/ViewAllDoActivities.swift
@@ -3,13 +3,12 @@ import SCTokens
 import SCComponents
 
 public struct ViewAllDoActivities: View {
-   
     @StateObject var viewModel: DoActivitiesViewModel = DoActivitiesViewModel()
     private var tapOnActivity:((_ data: DoActivitiesModel) -> Void)?
     public init(_ tapOnActivity:((_ data: DoActivitiesModel) -> Void)? = nil) {
         self.tapOnActivity = tapOnActivity
     }
-    
+
     public var body: some View {
         Group{
             switch viewModel.loadingState.viewLoadingState {
@@ -30,18 +29,17 @@ public struct ViewAllDoActivities: View {
             viewModel.fetchChildrenActivities(limit: 10, offset: 0)
         }
     }
-    
-    private var activityList: some View {
-        List(viewModel.activities) { item in
-            ActivityCardView(ActivityCardViewModel(activity: item))
-                .listRowSeparator(.hidden)
-                .onTapGesture {
-                    tapOnActivity?(item)
-                }
-                
-        }
-        .listStyle(.plain)
 
+    private var activityList: some View {
+        ScrollView(.vertical) {
+            ForEach(viewModel.activities) { item in
+                ActivityCardView(ActivityCardViewModel(activity: item))
+                    .onTapGesture {
+                        tapOnActivity?(item)
+                    }
+            }
+        }
+        .scrollIndicators(.hidden)
     }
 }
 
